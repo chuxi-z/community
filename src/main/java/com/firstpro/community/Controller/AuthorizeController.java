62aa938431e56147f6896a797088ffe496115d56
ChuxiZhang <55220214+chuxi-z@users.noreply.github.com>
2020-07-04 17:43:59 -0400

    log out and edit

--- src/main/java/com/firstpro/community/Controller/AuthorizeController.java
+++ src/main/java/com/firstpro/community/Controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import com.firstpro.community.dto.AccessTokenDTO;
 import com.firstpro.community.dto.GithubUser;
 import com.firstpro.community.mapper.UserMapper;
 import com.firstpro.community.model.User;
+import com.firstpro.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -28,6 +29,9 @@ public class AuthorizeController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private UserService userService;
+
     @Value("${github.client.id}")
     private String clientID;
     @Value("${github.client.secret}")
@@ -50,20 +54,16 @@ public class AuthorizeController {
         GithubUser githubUser = githubProvider.getUser(accessToken);
 
         if(githubUser != null && githubUser.getId() != null){//log success
-//            System.out.println("success");
             User user = new User();
             String token = UUID.randomUUID().toString();
             user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
-            user.setGmtCreate(System.currentTimeMillis());
-            user.setGmtModified(user.getGmtCreate());
             user.setAvatarUrl(githubUser.getAvatar_url());
-            userMapper.insert(user);
+            userService.createOrUpdate(user);
             Cookie cookie = new Cookie("token", token);
             cookie.setPath("/");
             response.addCookie(cookie);//写入cookie
-
 //            request.getSession().setAttribute("user", githubUser);//自动获取cookie
             return "redirect:/";
         }
@@ -72,5 +72,15 @@ public class AuthorizeController {
             return "redirect:/";
         }
 
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request,
+                         HttpServletResponse response){
+        request.getSession().removeAttribute("user");
+        Cookie cookie = new Cookie("token", null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:/";
     }
 }
--- src/main/java/com/firstpro/community/service/UserService.java
+++ src/main/java/com/firstpro/community/service/UserService.java
@@ -1,4 +1,29 @@
 package com.firstpro.community.service;
 
+import com.firstpro.community.mapper.UserMapper;
+import com.firstpro.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+
+    public void createOrUpdate(User user) {
+         User dbUser =  userMapper.findByAccountId(user.getAccountId());
+        if(dbUser == null){//insert
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+        }
+        else{//update
+            dbUser.setGmtModified(System.currentTimeMillis());
+            dbUser.setAvatarUrl(user.getAvatarUrl());
+            dbUser.setName(user.getName());
+            dbUser.setToken(user.getToken());
+            userMapper.update(dbUser);
+        }
+    }
 }
